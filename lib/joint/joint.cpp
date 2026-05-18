b91165304d0526757dcc94f754f58d1a68e627a7
BG <bg2286@student.uni-lj.si>
2026-05-18 23:39:28 +0200

    Refactoring joint class for robbustness and clarity (added limit checks, and structs to hold dynamic info and static at runtime)

--- lib/joint/joint.cpp
+++ lib/joint/joint.cpp
@@ -3,11 +3,23 @@
 #include <cmath>
 
 Joint::Joint(uint8_t step_pin, uint8_t dir_pin, uint8_t microsteps, float gear_ratio, float min_angle_rad, float max_angle_rad)
-  : _min_angle_rad(min_angle_rad),
-    _max_angle_rad(max_angle_rad),
-    _gear_ratio(gear_ratio),
-    _motor(step_pin, dir_pin, microsteps) {
-}
+  :   _motor(step_pin, dir_pin, microsteps)
+  {
+      _jointConfig.gear_ratio = gear_ratio;
+      _jointConfig.min_position = min_angle_rad;
+      _jointConfig.max_position = max_angle_rad;
+      _jointConfig.max_speed = 0.0f;
+      _jointConfig.max_acceleration = 0.0f;
+
+      _jointState.angle_rad = 0.0f;
+      _jointState.angle_vel_rad_s = 0.0f;
+      _jointState.angle_acc_rad_s2 = 0.0f;
+      _jointState.target_angle_rad = 0.0f;
+      _jointState.target_angle_vel_rad_s = 0.0f;
+      _jointState.moving = false;
+      _jointState.at_min_lim = false;
+      _jointState.at_max_lim = false;
+    }
 
 // ─────────────────────────────────────────────────────────────
 // Public functions
@@ -15,91 +27,36 @@ Joint::Joint(uint8_t step_pin, uint8_t dir_pin, uint8_t microsteps, float gear_r
 
 void Joint::init() {
   _motor.init();
-  _steps_per_joint_rev = _gear_ratio * _motor.getStepsPerRev();
-
-  _curr_angle = 0.0f;
-  _curr_speed = 0.0f;
-  
-  _start_angle = 0.0f;
-  _target_angle = 0.0f;
-  _target_speed = 0.0f;
-  _max_speed = 0.0f;
-  _max_acceleration = 0.0f;
-  _is_moving = false;
-
-  _position_tolerance = 0.005f;
-
-  setMaxSpeed(1.0f);
-  setMaxAcceleration(2.5f);
+  _jointConfig.steps_per_joint_rev = _jointConfig.gear_ratio * _motor.getStepsPerRev();
+  _jointState.joint_motion_control_paradigm = SPEED_CONTROL;
+  _jointConfig.position_tolerance = 0.005f;
 }
 
 void Joint::update(void) {
-  // TODO: add angle checks for max joint limits
-   // Refresh current state
-  _curr_angle = stepsToAngleRad(_motor.getCurrPosition());
-  _curr_speed = stepsPerSecToAngleVelRad(_motor.getCurrSpeed());
+  // Refresh current state
+  _jointState.angle_rad = stepsToAngleRad(_motor.getCurrPosition());
+  _jointState.angle_vel_rad_s = stepsPerSecToAngleVelRad(_motor.getCurrSpeed());
+  _jointState.moving = _motor.isMoving();
 
-  if (_motion_control_paradigm == POSITION_CONTROL){
-    float remaining = _target_angle - _curr_angle;
-    float dir = (remaining >= 0) ? 1.0f : -1.0f;
-
-    float abs_remaining = fabsf(remaining);
-
-    // stopping distance from current velocity
-    float d_stop = (_curr_speed * _curr_speed) / (2 * _max_acceleration);
-    // acceleration zone threshold
-    float d_acc = (_max_speed * _max_speed) / (2 * _max_acceleration);
-    
-    float cmd_speed = 0.0f;
-    if (fabsf(_curr_angle - _target_angle) < _position_tolerance){
-      cmd_speed = 0.0;
-    }
-    else{
-      if (abs_remaining <= d_stop) {
-          // must decelerate
-          cmd_speed = sqrtf(2 * _max_acceleration * abs_remaining);
-      }
-      else if (abs_remaining < d_acc) {
-          // still can accelerate toward max speed
-          cmd_speed = _max_speed;
-      }
-      else {
-          // cruise
-          cmd_speed = _max_speed;
-      }
-    }
-    cmd_speed = cmd_speed * dir;
-    setTargetSpeed(cmd_speed);
-  }
-  else if (_motion_control_paradigm == SPEED_CONTROL){
-    float d_stop = (_curr_speed * _curr_speed) / (2 * _max_acceleration);
-    // stopping distance from current velocity
-    clampAbsFloat(_curr_speed, _max_speed);
-    // check if we can stop until reaching the limit
-    if (((fabs(_curr_angle) + d_stop) < _min_angle_rad) || ((fabs(_curr_angle) + d_stop) > _max_angle_rad)){
-      setTargetSpeed(0);
-    }
-  }
-  //Serial.printf("d_acc: %.2f | d_left: %.2f | d_curr: %.2f | d_target: %.2f | v_cmd: %.2f\n", d_acc, remaining, _curr_angle, _target_angle, cmd_speed)
+  Joint::checkLimits();
 
   _motor.update();
-  _is_moving = _motor.isMoving();
 }
 
 bool Joint::isMoving(void){
-  return _is_moving;
+  return _jointState.moving;
 }
 
 void Joint::moveToAngle(float angle){
   angle = clampAngleRad(angle);
-  _target_angle = angle;
-  _target_steps = angleRadToSteps(_target_angle);
-  _start_angle = _curr_angle;
+  _jointState.target_angle_rad = angle;
 }
 
 void Joint::setTargetSpeed(float angular_speed){
+  angular_speed = clampAbsFloat(angular_speed, _jointConfig.max_speed);
+  
   _motor.setTargetSpeed(angleVelRadToStepsPerSec(angular_speed));
-  _target_speed = angular_speed;
+  _jointState.target_angle_vel_rad_s = angular_speed;
 }
 
 void Joint::setMaxSpeed(float max_angular_speed){
@@ -108,7 +65,7 @@ void Joint::setMaxSpeed(float max_angular_speed){
     max_angular_speed = -max_angular_speed;
   }
   _motor.setMaxSpeed(angleVelRadToStepsPerSec(max_angular_speed));
-  _max_speed = max_angular_speed;
+  _jointConfig.max_speed = max_angular_speed;
 }
 
 void Joint::setMaxAcceleration(float max_angular_acceleration){
@@ -117,36 +74,24 @@ void Joint::setMaxAcceleration(float max_angular_acceleration){
     max_angular_acceleration = -max_angular_acceleration;
   }
   _motor.setMaxAcceleration(angleAccelRadToStepsPerSec2(max_angular_acceleration));
-  _max_acceleration = max_angular_acceleration;
+  _jointConfig.max_acceleration = max_angular_acceleration;
 }
 
-float Joint::getAngle(void){
-  return _curr_angle;
+JointState Joint::getState(void){
+  return _jointState;
 }
 
-float Joint::getTargetAngle(void){
-  return _target_angle;
-}
-
-float Joint::getSpeed(void){
-    return _curr_speed;
-}
-
-float Joint::getMaxSpeed(void){
-  return _max_speed;
-}
-
-float Joint::getMaxAcceleration(void){
-  return _max_acceleration;
+JointConfig Joint::getJointConfig(void){
+  return _jointConfig;
 }
 
 void Joint::stop(void){
   _motor.stop();
-  _target_speed = 0.0f;
+  _jointState.target_angle_vel_rad_s = 0.0f;
 }
 
-bool Joint::setMotionControlParadigm(motion_control_paradigm_t mode){
-  _motion_control_paradigm =  mode;
+bool Joint::setMotionControlParadigm(joint_motion_control_paradigm_t mode){
+  _jointState.joint_motion_control_paradigm =  mode;
   return true;
 }
 
@@ -155,31 +100,44 @@ bool Joint::setMotionControlParadigm(motion_control_paradigm_t mode){
 // ─────────────────────────────────────────────────────────────
 
 float Joint::clampAngleRad(float angle_rad) {
-  if (angle_rad < _min_angle_rad){
-    angle_rad = _min_angle_rad;
+  if (angle_rad < _jointConfig.min_position){
+    angle_rad = _jointConfig.min_position;
   }
-  if (angle_rad > _max_angle_rad){
-    angle_rad = _max_angle_rad;
+  if (angle_rad > _jointConfig.max_position){
+    angle_rad = _jointConfig.max_position;
   }
   return angle_rad;
 }
 
 float Joint::stepsToAngleRad(long steps) {
-  return (static_cast<float>(steps) / static_cast<float>(_steps_per_joint_rev)) * 2.0f * PI;
+  return (static_cast<float>(steps) / static_cast<float>(_jointConfig.steps_per_joint_rev)) * 2.0f * PI;
 }
 
 long Joint::angleRadToSteps(float angle_rad) {
-  return std::lround((angle_rad / (2.0f * PI)) * static_cast<float>(_steps_per_joint_rev));
+  return std::lround((angle_rad / (2.0f * PI)) * static_cast<float>(_jointConfig.steps_per_joint_rev));
 }
 
 long Joint::angleVelRadToStepsPerSec(float angle_rad_s) const {
-  return std::lround((angle_rad_s / (2.0f * PI)) * static_cast<float>(_steps_per_joint_rev));
+  return std::lround((angle_rad_s / (2.0f * PI)) * static_cast<float>(_jointConfig.steps_per_joint_rev));
 }
 
 float Joint::stepsPerSecToAngleVelRad(float steps_per_sec) const {
-  return (steps_per_sec / static_cast<float>(_steps_per_joint_rev)) * 2.0f * PI;
+  return (steps_per_sec / static_cast<float>(_jointConfig.steps_per_joint_rev)) * 2.0f * PI;
 }
 
 float Joint::angleAccelRadToStepsPerSec2(float angle_rad_s2) const {
-  return (angle_rad_s2 / (2.0f * PI)) * static_cast<float>(_steps_per_joint_rev);
+  return (angle_rad_s2 / (2.0f * PI)) * static_cast<float>(_jointConfig.steps_per_joint_rev);
+}
+
+void Joint::checkLimits(void){
+  _jointState.at_min_lim = (_jointState.angle_rad <= _jointConfig.min_position);
+  _jointState.at_max_lim = (_jointState.angle_rad >= _jointConfig.max_position);
+
+  if (_jointState.at_min_lim && _jointState.target_angle_vel_rad_s < 0.0f){
+    Joint::setTargetSpeed(0.0f);
+  }
+
+  if (_jointState.at_max_lim && _jointState.target_angle_vel_rad_s > 0.0f){
+    Joint::setTargetSpeed(0.0f);
+  }
 }
--- lib/joint/joint.h
+++ lib/joint/joint.h
@@ -5,71 +5,81 @@
 
 #define ACCEL_SAFETY_MARGIN 0.8f
 
-enum motion_control_paradigm_t {
+enum joint_motion_control_paradigm_t {
   SPEED_CONTROL,
   POSITION_CONTROL
 };
 
+struct JointConfig {
+  // Usually static and does not change during runtime
+  float min_position;
+  float max_position;
+
+  float max_speed;
+  float max_acceleration;
+
+  float gear_ratio;
+  long steps_per_joint_rev;
+  float position_tolerance;
+};
+
+struct JointState {
+  // Usually dynamic and does change during runtime
+  float angle_rad;
+  float angle_vel_rad_s;
+  float angle_acc_rad_s2;
+
+  float target_angle_rad;
+  float target_angle_vel_rad_s;
+
+  bool moving;
+  joint_motion_control_paradigm_t joint_motion_control_paradigm;
+  bool at_min_lim;
+  bool at_max_lim;
+};
+
 class Joint {
-public:
-  // Constructor angle limits are in rads
-  Joint(uint8_t step_pin,
-        uint8_t dir_pin,
-        uint8_t microsteps,
-        float gear_ratio,
-        float min_angle_rad,
-        float max_angle_rad);
+  public:
+    // Constructor angle limits are in rads
+    Joint(uint8_t step_pin,
+          uint8_t dir_pin,
+          uint8_t microsteps,
+          float gear_ratio,
+          float min_angle_rad,
+          float max_angle_rad);
 
-  void init(void);
-  void update(void);
-  bool isMoving(void);
-  void stop(void);
+    void init(void);
+    void update(void);
+    bool isMoving(void);
+    void stop(void);
 
-  void moveToAngle(float angle);
+    void moveToAngle(float angle);
 
-  void setTargetSpeed(float angular_speed);
-  void setMaxSpeed(float max_angular_speed);
-  void setMaxAcceleration(float max_angular_acceleration);
-  bool setMotionControlParadigm(motion_control_paradigm_t mode);
- 
-  float getAngle(void);
-  float getTargetAngle(void);
-  float getSpeed(void);
-  float getMaxSpeed(void);
-  float getMaxAcceleration(void);
-
-private:
-  float _min_angle_rad;
-  float _max_angle_rad;
-  float _gear_ratio;
-
-  motion_control_paradigm_t _motion_control_paradigm;
-  long _steps_per_joint_rev;
-  bool _is_moving;
-  float _position_tolerance;
-
-  float _start_angle;
-  float _target_angle;
-  long _target_steps;
-  float _target_speed;
-  float _cmd_speed;
-
-  float _curr_angle;
-  float _curr_speed;
-  float _max_acceleration;
-  float _max_speed;
-
-  Motor _motor;
-
-  float stepsToAngleRad(long steps);
-  long angleRadToSteps(float angle_rad);
+    void setTargetSpeed(float angular_speed);
+    void setMaxSpeed(float max_angular_speed);
+    void setMaxAcceleration(float max_angular_acceleration);
+    bool setMotionControlParadigm(joint_motion_control_paradigm_t mode);
   
-  long  angleVelRadToStepsPerSec(float angle_rad_s) const;
-  float stepsPerSecToAngleVelRad(float steps_per_sec) const;
-  
-  float angleAccelRadToStepsPerSec2(float angle_rad_s2) const;
+    JointState getState(void);
+    JointConfig getJointConfig(void);
 
-  float clampAngleRad(float angle_rad);
+  private:
+    JointConfig _jointConfig;
+    JointState _jointState;
+
+    Motor _motor;
+
+    float stepsToAngleRad(long steps);
+    long angleRadToSteps(float angle_rad);
+    
+    long  angleVelRadToStepsPerSec(float angle_rad_s) const;
+    float stepsPerSecToAngleVelRad(float steps_per_sec) const;
+    
+    float angleAccelRadToStepsPerSec2(float angle_rad_s2) const;
+
+    float clampAngleRad(float angle_rad);
+
+    void checkLimits(void);
 };
 
 #endif
