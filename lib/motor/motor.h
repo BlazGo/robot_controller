dabb59c53744ebe2964e85392fb44fc5411e80b9
BG <bg2286@student.uni-lj.si>
2026-05-28 10:13:39 +0200

    moved structs for state and config to separate file

--- lib/motor/motor.h
+++ lib/motor/motor.h
@@ -3,30 +3,16 @@
 
 #include <Arduino.h>
 #include <AccelStepper.h>
+#include "motor_types.h"
 
 static constexpr long STEPPER_STEPS_PER_REV = 200L;
-static constexpr long MAX_SPEED_STEPS_PER_S = 100L;
-static constexpr long MAX_ACCELERATION_STEPS_PER_S2 = 500L;
-static constexpr float kSpeedEpsilon = 0.01f;
+static constexpr long MAX_SPEED_STEPS_PER_S = 4000L;
+static constexpr long MAX_ACCELERATION_STEPS_PER_S2 = 8000L;
+static constexpr float kSpeedEpsilon = 0.0025f;
 
-struct MotorConfig {
-    uint8_t stepPin = 0U;
-    uint8_t directionPin = 0U;
-    uint8_t microsteps = 1U;
-    long stepsPerRevolution = 0L;
-    float maxSpeedSteps = 0.0f;
-    float maxAccelerationSteps = 0.0f;
-};
-
-struct MotorState {
-    long currentPositionSteps = 0L;
-    float currentSpeedSteps = 0.0f;
-    float targetSpeedSteps = 0.0f;
-    bool isMoving = false;
-};
 
 class Motor {
-public:
+    public:
     Motor(uint8_t stepPin, uint8_t directionPin, uint8_t microsteps);
 
     void initialize(void);
